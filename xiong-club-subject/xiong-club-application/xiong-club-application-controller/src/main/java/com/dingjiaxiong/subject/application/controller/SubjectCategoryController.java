28a200130e92be0c2ed69d3175733c144cd96949
DingJiaxiong <1612102142@qq.com>
2024-08-13 14:46:57 +0800

    precondions的参数校验

--- xiong-club-subject/xiong-club-application/xiong-club-application-controller/src/main/java/com/dingjiaxiong/subject/application/controller/SubjectCategoryController.java
+++ xiong-club-subject/xiong-club-application/xiong-club-application-controller/src/main/java/com/dingjiaxiong/subject/application/controller/SubjectCategoryController.java
@@ -6,7 +6,9 @@ import com.dingjiaxiong.subject.application.dto.SubjectCategoryDTO;
 import com.dingjiaxiong.subject.common.entity.Result;
 import com.dingjiaxiong.subject.domain.entity.SubjectCategoryBO;
 import com.dingjiaxiong.subject.domain.service.SubjectCategoryDomainService;
+import com.google.common.base.Preconditions;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -44,12 +46,18 @@ public class SubjectCategoryController {
                 log.info("SubjectCategoryController.add.dto:{}", JSON.toJSONString(subjectCategoryDTO));
             }
 
+            // 参数校验
+            Preconditions.checkNotNull(subjectCategoryDTO.getCategoryType(), "分类类型不能为空");
+            Preconditions.checkArgument(!StringUtils.isBlank(subjectCategoryDTO.getCategoryName()), "分类名称不能为空");
+            Preconditions.checkNotNull(subjectCategoryDTO.getParentId(), "分类父级id不能为空");
+
             SubjectCategoryBO subjectCategoryBO = SubjectCategoryDTOConverter.INSTANCE.convertDtoToCategoryBO(subjectCategoryDTO);
             subjectCategoryDomainService.add(subjectCategoryBO);
 
             return Result.ok(true);
 
         } catch (Exception e) {
+            log.error("SubjectCategoryController.add.error:{}", e.getMessage(), e);
             return Result.fail();
         }
 
--- xiong-club-subject/xiong-club-application/xiong-club-application-controller/target/generated-sources/annotations/com/dingjiaxiong/subject/application/convert/SubjectCategoryDTOConverterImpl.java
+++ xiong-club-subject/xiong-club-application/xiong-club-application-controller/target/generated-sources/annotations/com/dingjiaxiong/subject/application/convert/SubjectCategoryDTOConverterImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2024-08-13T14:35:36+0800",
+    date = "2024-08-13T14:45:26+0800",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 1.8.0_411 (Oracle Corporation)"
 )
 public class SubjectCategoryDTOConverterImpl implements SubjectCategoryDTOConverter {

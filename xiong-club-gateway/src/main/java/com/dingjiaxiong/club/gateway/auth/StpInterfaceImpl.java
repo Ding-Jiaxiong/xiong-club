d9ecc96ff50ec0ebaa2bab7dbb25c1f0610b8096
DingJiaxiong <1612102142@qq.com>
2024-08-14 21:56:51 +0800

    gateway 实现redis 拉取权限信息

--- xiong-club-gateway/src/main/java/com/dingjiaxiong/club/gateway/auth/StpInterfaceImpl.java
+++ xiong-club-gateway/src/main/java/com/dingjiaxiong/club/gateway/auth/StpInterfaceImpl.java
@@ -2,6 +2,8 @@ package com.dingjiaxiong.club.gateway.auth;
 
 import cn.dev33.satoken.stp.StpInterface;
 import com.alibaba.cloud.commons.lang.StringUtils;
+import com.dingjiaxiong.club.gateway.redis.RedisUtil;
+import com.google.gson.Gson;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
@@ -19,21 +21,39 @@ import java.util.stream.Collectors;
 @Component
 public class StpInterfaceImpl implements StpInterface {
 
+    @Resource
+    private RedisUtil redisUtil;
+
+    private String authPermissionPrefix = "auth.permission";
+    //
+    private String authRolePrefix = "auth.role";
+
+//    private String authPrefix = "auth";
+
     // 返回此 loginId 拥有的权限列表
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
 
-        LinkedList<String> permissionList = new LinkedList<>();
-        permissionList.add("subject:add");
-        return permissionList;
+        return getAuth(loginId.toString(), authPermissionPrefix);
     }
 
     // 返回此 loginId 拥有的角色列表
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-        LinkedList<String> roleList = new LinkedList<>();
-        roleList.add("admin");
-        return roleList;
+        return getAuth(loginId.toString(), authRolePrefix);
+    }
+
+    private List<String> getAuth(String loginId, String prefix) {
+        String authKey = redisUtil.buildKey(prefix, loginId.toString());
+        String authValue = redisUtil.get(authKey);
+
+        if (StringUtils.isBlank(authValue)) {
+            return Collections.emptyList();
+        }
+
+        List<String> authList = new Gson().fromJson(authValue, List.class);
+
+        return authList;
     }
 
 }

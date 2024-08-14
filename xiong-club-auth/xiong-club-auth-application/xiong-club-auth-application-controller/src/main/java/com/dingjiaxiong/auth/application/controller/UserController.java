94868f0a40fb5975551dc12be0ffcb02a22e5570
DingJiaxiong <1612102142@qq.com>
2024-08-14 18:37:09 +0800

    satoken实现登录及token机制

---
+++ xiong-club-auth/xiong-club-auth-application/xiong-club-auth-application-controller/src/main/java/com/dingjiaxiong/auth/application/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.dingjiaxiong.auth.application.controller;
+
+import cn.dev33.satoken.stp.SaTokenInfo;
+import cn.dev33.satoken.stp.StpUtil;
+import cn.dev33.satoken.util.SaResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/user/")
+public class UserController {
+
+    // 测试登录，浏览器访问： http://localhost:8081/user/doLogin?username=zhang&password=123456
+    @RequestMapping("doLogin")
+    public SaResult doLogin(String username, String password) {
+        // 此处仅作模拟示例，真实项目需要从数据库中查询数据进行比对
+        if ("zhang".equals(username) && "123456".equals(password)) {
+            StpUtil.login(10001);
+
+            SaTokenInfo tokenInfo = StpUtil.getTokenInfo();
+
+            return SaResult.data(tokenInfo);
+        }
+        return SaResult.error("登录失败");
+    }
+
+    // 查询登录状态，浏览器访问： http://localhost:8081/user/isLogin
+    @RequestMapping("isLogin")
+    public String isLogin() {
+        return "当前会话是否登录：" + StpUtil.isLogin();
+    }
+
+}

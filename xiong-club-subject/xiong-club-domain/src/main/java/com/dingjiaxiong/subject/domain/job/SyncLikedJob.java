45723ad0928b71498d4cf33de91faca7224769f8
DingJiaxiong <1612102142@qq.com>
2024-08-17 13:40:48 +0800

    xxljob定时任务集成

---
+++ xiong-club-subject/xiong-club-domain/src/main/java/com/dingjiaxiong/subject/domain/job/SyncLikedJob.java
@@ -0,0 +1,48 @@
+package com.dingjiaxiong.subject.domain.job;
+
+import com.dingjiaxiong.subject.domain.service.SubjectLikedDomainService;
+import com.xxl.job.core.context.XxlJobHelper;
+import com.xxl.job.core.handler.annotation.XxlJob;
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Arrays;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 同步点赞数据
+ *
+ * @author: Ding Jiaxiong
+ * @date: 2024/1/8
+ */
+@Component
+@Slf4j
+public class SyncLikedJob {
+
+    @Resource
+    private SubjectLikedDomainService subjectLikedDomainService;
+
+    /**
+     * 同步点赞数据任务
+     */
+    @XxlJob("syncLikedJobHandler")
+    public void syncLikedJobHandler() throws Exception {
+        XxlJobHelper.log("syncLikedJobHandler.start");
+        try {
+            subjectLikedDomainService.syncLiked();
+        } catch (Exception e) {
+            XxlJobHelper.log("syncLikedJobHandler.error" + e.getMessage());
+        }
+    }
+
+}
